9be68e94b02a563fcee457430c832e6c1c845792
Kartik Lunkad <kartiklunkad@kartiklunkad-kq1tpfqqjm.myfiosgateway.com>
2023-02-13 23:35:41 -0500

    Using .NET SDK

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using Vault;
+using Vault.Client;
+using Vault.Model;
+
+namespace Example
+{
+    public class Example
+    {
+        public static void Main()
+        {
+            string address = Environment.GetEnvironmentVariable("VAULT_ADDR");
+            VaultConfiguration config = new VaultConfiguration(address);
+
+            VaultClient vaultClient = new VaultClient(config);
+            vaultClient.SetToken(Environment.GetEnvironmentVariable("VAULT_TOKEN"));
+
+            try 
+            {    
+                var secretData = new Dictionary<string, string> { { "mypass", "pass" } };
+
+                // Write a secret
+                var kvRequestData = new KVv2WriteRequest(secretData);
+
+                vaultClient.Secrets.KVv2Write("mypath", kvRequestData);
+
+                // Read a secret
+                VaultResponse<Object> resp = vaultClient.Secrets.KVv2Read("mypath");
+                Console.WriteLine(resp.Data);
+            }
+            catch (VaultApiException e)
+            {
+                Console.WriteLine("Failed to read secret with message {0}", e.Message);
+            }
+        }
+    }
+}
